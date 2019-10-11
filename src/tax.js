65158054a3f9d430f93c635123caf16fbb3ac0f9
sushma95 <54374529+sushma95@users.noreply.github.com>
2019-10-11 09:29:12 -0500

    area calc

---
+++ src/tax.js
@@ -0,0 +1,40 @@
+function calculations() {
+    
+  //Passes string and returns float
+const top = parseFloat(document.querySelector('#firstNumber').value)
+  const bottom = parseFloat(document.querySelector('#secondNumber').value)
+  const height = parseFloat(document.querySelector('#thirdNumber').value)
+  
+  if(top<0){
+      throw new Error( "top shouldn't be negative");
+  }
+  if(bottom<0){
+      throw new Error( "bottom shouldn't be negative");
+  }
+if(height<0){
+      throw new Error( "height shouldn't be negative");
+  }
+
+  //calculating the area
+  const area=((top+bottom)*height)/2;
+
+//  const top1 = document.querySelector("#firstNumber").value
+//   const bottom1 = document.querySelector("#secondNumber").value
+//   const height1 = document.querySelector("#thirdNumber").value
+
+  localStorage.setItem('top1', top)
+  localStorage.setItem('bottom1', bottom)
+  localStorage.setItem('height1', height)
+
+  //Modifying HTML page with the calculations output
+  document.querySelector('#result').innerHTML = `${area}`
+}
+
+function testcalculations(top, bottom,height){
+  return ((top+bottom)*height)/2
+}
+
+//On click perform the calculation function
+document.querySelector('#areatrap').addEventListener('click', calculations)
+
+
---
+++ src/tax.test.js
@@ -0,0 +1,33 @@
+QUnit.module('Main', {})
+
+QUnit.test( "Test area of trapezium", function( assert ) {
+    assert.equal(testcalculations(1, 1, 1), 1, "calculating the area of trapezoid gives the value 1 with the given inputs")
+    assert.equal(testcalculations(2, 2, 2.5), 5, "calculating the area of trapezoid gives the value 5 with the given inputs")
+    assert.equal(testcalculations(2, 1, 2), 3, "calculating the area of trapezoid gives the value 3 with the given inputs")
+    assert.equal(testcalculations(1, 4, 3), 7.5, "calculating the area of trapezoid gives the value 7.5 with the given inputs")
+    assert.equal(testcalculations(1, 4, 2), 5, "calculating the area of trapezoid gives the value 5 with the given inputs")
+})
+window.addEventListener('load', () => {
+    const appURL = '../index.html' 
+    const openingTag = '<main class="container mt-5 flex-fill">'
+    const closingTag = '</main>' // go grab it!
+    fetch(appURL, { method: 'GET' })
+      .then(response => {
+        return response.text() // returns promise
+      })
+      .then(txt => {                
+        const start = txt.indexOf(openingTag)
+        const end = txt.indexOf(closingTag) + closingTag.length
+        const html = txt.substring(start, end) // we only want part of the page
+        const qunitFixtureBody = document.querySelector('#qunit-fixture')
+        qunitFixtureBody.innerHTML = html // put the page into the DOM - the second div associated with this page
+        console.info(qunitFixtureBody) // print it out so we can see it (it doesn't get inserted into the page)
+        QUnit.start() // start the actual testing - it finds and runs both the tests, defined in QUnit.test()
+      })
+      .catch(error => {
+        console.error('error:', error);
+        QUnit.start()
+      })
+  })
+
+  
